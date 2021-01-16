0b8bd0bd368c638e34d7518b128452c4c3884371
bal3000 <bal3000286@googlemail.com>
2021-01-16 14:31:22 +0000

    schedule handler skeleton

---
+++ handlers/schedulehandler.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/bal3000/BalStreamer.API/helpers"
+	"github.com/labstack/echo/v4"
+)
+
+// ScheduleHandler is the handler struct for schedule endpoints
+type ScheduleHandler struct {
+	RabbitMQ *helpers.RabbitMQ
+}
+
+// NewScheduleHandler creates a new pointer to schedule
+func NewScheduleHandler(rabbit *helpers.RabbitMQ) *ScheduleHandler {
+	return &ScheduleHandler{RabbitMQ: rabbit}
+}
+
+// AddEventToSchedule sends the event to the schedule app and logs a copy
+func (handler *ScheduleHandler) AddEventToSchedule(c echo.Context) error {
+	// Get info from post object and create a rabbit message
+
+	// Send message to rabbit and also save to db if needed
+
+	// Return success
+	return c.String(http.StatusOK, "Successfully added event to schedule")
+}
